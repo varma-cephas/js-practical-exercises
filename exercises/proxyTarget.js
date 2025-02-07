fbe7a56b447fc614c0e1e3c3c34577ed1fea60a2
Gym Ubutwari <gymubutwari@Ubutwaris-iMac-2.local>
2025-02-07 14:04:23 +0200

    add useful links for working with proxies

--- exercises/proxyTarget.js
+++ exercises/proxyTarget.js
@@ -19,3 +19,11 @@ const proxy2 = new Proxy(target, handler2);
 proxy2.message2=0;
 console.log(proxy2.message2)
 console.log(target.message2)
+
+//super helpful proxy links
+/*
+https://dev.to/wizdomtek/unleashing-the-power-of-javascript-proxy-a-comprehensive-introduction-for-developers-3f4j
+https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Proxy
+https://tr.javascript.info/proxy#:~:text=receiver%20%E2%80%93%20if%20the%20property%20is,if%20we%20inherit%20from%20proxy).
+https://babeljs.io/docs/learn
+*/
